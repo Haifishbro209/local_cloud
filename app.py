2eee4f54554ed9300c84a1250bce18c441b0ff51
haifishbro209 <kaloyannikolov016@gmail.com>
2025-05-22 08:29:19 +0200

    some error when downloading

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import *
 
 app = Flask(__name__)
 
+
+files = [{"name":"wow","size": "10MB", "upload_time":"13:00", "download_url":"/download"}]
+
 @app.route("/")
-def landingpage():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", files = files)
+
+@app.route("/download/<name>")
+def download(name):
+    path = f"/storage/{name}"
+    return send_file(path, as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
